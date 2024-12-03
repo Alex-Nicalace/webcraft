3aae6e02b2307eaba8d1e4a199a79e8e9d29844e
Alex-Nicalace <anicalace@gmail.com>
2024-12-03 10:04:53 +0200

    Icon: add icon "down"

--- src/components/ui/Icon/icons.ts
+++ src/components/ui/Icon/icons.ts
@@ -1,3 +1,4 @@
 export { default as ArrowRight } from '../../../assets/icons/arrow_right.svg?react';
 export { default as DarkTheme } from '../../../assets/icons/dark_theme.svg?react';
 export { default as LightTheme } from '../../../assets/icons/light_theme.svg?react';
+export { default as Dawn } from '../../../assets/icons/dawn.svg?react';
