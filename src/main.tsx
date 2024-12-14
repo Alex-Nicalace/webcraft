9247ccf11db71c420bd0854f8b5dad3d5a2cc99d
Alex-Nicalace <anicalace@gmail.com>
2024-12-14 12:10:32 +0200

    ErrorBoundary: usage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import './styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      fallback={
+        <main
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          ⚠️ Во время отрисовки страницы произошла ошибка ‼️{' '}
+        </main>
+      }
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
