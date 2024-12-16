2d65fe384b922a05a7dbd0739e1d88bd21fc22e3
Alex-Nicalace <anicalace@gmail.com>
2024-12-16 14:15:10 +0200

    Container: usage forwardRef

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,18 +1,29 @@
-import { createElement } from 'react';
+import { createElement, forwardRef } from 'react';
 import { TContainerProps } from './Container.types';
 import './Container.scss';
 
-function Container<T extends keyof JSX.IntrinsicElements>({
-  children,
-  tag,
-  className,
-  ...props
-}: TContainerProps<T>): JSX.Element {
-  return createElement(
-    tag,
-    { ...props, className: [className, 'container'].filter(Boolean).join(' ') },
-    children
-  );
-}
+const Container = forwardRef(
+  <T extends keyof JSX.IntrinsicElements>(
+    { children, tag, className, ...props }: TContainerProps<T>,
+    ref: React.Ref<
+      T extends keyof HTMLElementTagNameMap
+        ? HTMLElementTagNameMap[T]
+        : SVGElement
+    > // Типизация ref
+  ) => {
+    return createElement(
+      tag,
+      {
+        ...props,
+        className: [className, 'container'].filter(Boolean).join(' '),
+        ref, // Передача ref
+      },
+      children
+    );
+  }
+);
+// Устанавливаем displayName без этого ESLint ругается, т.к.
+// без этого во время отладки имя компонента станвится ForwardRef
+Container.displayName = 'Container';
 
 export default Container;
