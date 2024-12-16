3c8d39a71f1dc7a5ffca398405994621dc0ccdb0
Alex-Nicalace <anicalace@gmail.com>
2024-12-16 08:34:59 +0200

    Header: refactoring

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './Header.scss';
 import Container from '../Container';
 import Logo from '../Logo';
@@ -8,6 +8,7 @@ import TogglerTheme from '../ui/TogglerTheme';
 import { useScreenWidth } from '../../Context/ScreenWidthContext';
 import BurgerButton from '../BurgerButton';
 import { useDarkMode } from '../../Context/DarkModeContext';
+import { usePageScrolled } from '../../hooks/usePageScrolled';
 
 const MENU_ITEMS = [
   { label: 'Обо мне', href: '#section1' },
@@ -18,44 +19,15 @@ const MENU_ITEMS = [
 // type THeaderProps = { }
 function Header(/*{ }: THeaderProps*/): JSX.Element {
   const [isMenuOpen, setMenuOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
   const { isLessTablet, isLessMobileMd, isLessMobileSmall } = useScreenWidth();
   const { isDarkMode, toggleMode } = useDarkMode();
+  const scrolledPage = usePageScrolled();
   const isMobileMenu = isLessTablet;
-
-  useEffect(function manageScrolledState() {
-    // Первоначально реализовал с помощью window.addEventListener('scroll', ...)
-    // в колбэке отслеживал значение window.scrollY и колбэк обернул в throttle.ts
-    // Но решил попрактиковаться с IntersectionObserver
-    const options = {
-      root: null, // Наблюдаем за viewport
-      threshold: 0, // Пересечение фиксируется мгновенно
-      // Область наблюдения. Верхнюю границу поднял примерно на высоту хедера
-      // а нижнюю поднял на всю высоту экрана вверх.
-      // Т.о. получилась область наблюдения вне видимой части экрана сверху полосой в 60px
-      rootMargin: '60px 0px -100.2% 0px',
-    };
-
-    const callback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        setScrolled(entry.isIntersecting);
-      });
-    };
-
-    const observer = new IntersectionObserver(callback, options);
-    // Ищем первый элемент после хедера, т.к. после него контент котрый и будет скроллиться
-    const contentElem = document.querySelector('.header + *');
-    if (contentElem) observer.observe(contentElem);
-
-    return () => {
-      if (contentElem) observer.unobserve(contentElem);
-    };
-  });
 
   return (
     <Container
       tag="header"
-      className={['header', scrolled && 'header_scrolled']
+      className={['header', scrolledPage && 'header_scrolled']
         .filter(Boolean)
         .join(' ')}
     >
