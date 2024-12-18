d1865782dfb0b8b7e37e93bfb908ee5bd03058a9
Alex-Nicalace <anicalace@gmail.com>
2024-12-18 16:06:26 +0200

    Greeting: add Button

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -3,6 +3,7 @@ import Container from '../Container';
 import { TGreetingProps } from './Greeting.types';
 import PuzzleSvg from '../../assets/img/puzzles/puzzle-main-screen.svg?react';
 import HeadingSvg from '../../assets/img/puzzles/puzzle-main-screen-text-bg.svg?react';
+import Button from '../ui/Button';
 
 function Greeting({ className }: TGreetingProps): JSX.Element {
   return (
@@ -22,6 +23,13 @@ function Greeting({ className }: TGreetingProps): JSX.Element {
           <HeadingSvg className="greeting__mask" />
         </div>
       </div>
+      <Button
+        href="#portfolio"
+        className="greeting__button"
+        variant="button-secondary"
+      >
+        Смотреть портфолио
+      </Button>
     </Container>
   );
 }
