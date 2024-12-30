3985330b3c233731c9c2969fe2abef93fcdb64e3
Alex-Nicalace <anicalace@gmail.com>
2024-12-30 13:59:50 +0200

    Greeting: completed

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import './Greeting.scss';
 import Container from '../Container';
 import { TGreetingProps } from './Greeting.types';
 import PuzzleSvg from '../../assets/img/puzzles/puzzle-main-screen.svg?react';
 import HeadingSvg from '../../assets/img/puzzles/puzzle-main-screen-text-bg.svg?react';
 import Button from '../ui/Button';
-import { CODE_DEKOR } from './codeDekor';
+import { CODE_ANIMATED, CODE_STATIC } from './codeDecor';
 import Dawn from '../ui/Dawn';
-import { useState } from 'react';
+import { useDarkMode } from '../../Context/DarkModeContext';
+import { useScreenWidth } from '../../Context/ScreenWidthContext';
 
 function Greeting({ className }: TGreetingProps): JSX.Element {
+  const { isDarkMode } = useDarkMode();
+  const { isLessPC } = useScreenWidth();
   const [isAnimatedDecor, setIsAnimatedDecor] = useState(false);
-  const [isStopAnimateBall, setIsStopAnimateBall] = useState(false);
+  const [isStopedAnimatePuzzle, setIsStopedAnimatePuzzle] = useState(false);
+  const [animationType, setAnimationType] = useState<'ball' | 'code'>('code');
   const BallCount = 3;
+  const codeAnimated = !isLessPC ? CODE_ANIMATED : CODE_ANIMATED.slice(0, 2);
 
-  function handleToggleAnimateBall() {
-    setIsStopAnimateBall((prev) => !prev);
+  function handleToggleAnimatePuzzle() {
+    setIsStopedAnimatePuzzle((prev) => !prev);
+  }
+
+  function handleToggleAnimationType(
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    if (e.target !== e.currentTarget || isDarkMode) return;
+    setAnimationType((prev) => (prev === 'ball' ? 'code' : 'ball'));
+    setIsStopedAnimatePuzzle(false);
   }
 
   return (
@@ -36,21 +50,34 @@ function Greeting({ className }: TGreetingProps): JSX.Element {
         <p className="greeting__text heading-additional-liquid">
           <span>Привет!</span>
         </p>
-        <div className="greeting__puzzle">
-          {Array(BallCount)
-            .fill(null)
-            .map((_, index) => (
-              <span
-                key={index}
-                className={[
-                  `greeting__ball greeting__ball_${index + 1}`,
-                  isStopAnimateBall && 'greeting__ball_stoped',
-                ]
-                  .filter(Boolean)
-                  .join(' ')}
-                onClick={handleToggleAnimateBall}
-              ></span>
-            ))}
+        <div
+          className={[
+            'greeting__puzzle',
+            isStopedAnimatePuzzle && 'greeting__puzzle_stoped-animation',
+          ]
+            .filter(Boolean)
+            .join(' ')}
+          onClick={handleToggleAnimationType}
+        >
+          {animationType === 'code' && !isDarkMode
+            ? codeAnimated.map((code, i) => (
+                <pre
+                  key={i}
+                  className={`greeting__animated-code greeting__animated-code_${i + 1} text-typing`}
+                  onClick={handleToggleAnimatePuzzle}
+                >
+                  <code>{code}</code>
+                </pre>
+              ))
+            : Array(BallCount)
+                .fill(null)
+                .map((_, index) => (
+                  <span
+                    key={index}
+                    className={`greeting__ball greeting__ball_${index + 1}`}
+                    onClick={handleToggleAnimatePuzzle}
+                  ></span>
+                ))}
           <PuzzleSvg className="greeting__mask" />
           <HeadingSvg className="greeting__mask" />
         </div>
@@ -59,7 +86,7 @@ function Greeting({ className }: TGreetingProps): JSX.Element {
             className="greeting__decor"
             onAnimationEnd={() => setIsAnimatedDecor(true)}
           >
-            {CODE_DEKOR.map((code, i) => (
+            {CODE_STATIC.map((code, i) => (
               <pre
                 key={i}
                 className={`greeting__code greeting__code_${i + 1} text-typing`}
