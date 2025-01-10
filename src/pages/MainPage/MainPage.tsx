a46267fd9c5d6072528a0fba853ea4f41ccf8cd3
Alex-Nicalace <anicalace@gmail.com>
2025-01-10 12:07:32 +0200

    MainPage: usage ProjectList

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,8 @@ import AboutMe from '../../components/AboutMe';
 import Competencies from '../../components/Competencies';
 import Credo from '../../components/Credo/Credo';
 import Greeting from '../../components/Greeting';
+import ProjectList from '../../components/ui/ProjectList';
+import { PROJECTS } from './MainPage.config';
 import './MainPage.scss';
 
 // type TMainPageProps = { }
@@ -12,6 +14,7 @@ function MainPage(/*{ }: TMainPageProps*/): JSX.Element {
       <AboutMe className="main-page__about-me" />
       <Competencies className="main-page__competencies" />
       <Credo className="main-page__credo" />
+      <ProjectList className="main-page__projects" data={PROJECTS} />
     </main>
   );
 }
