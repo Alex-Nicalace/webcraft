cc4e8e80a44e6408d494f98baa153598960d6533
Alex-Nicalace <anicalace@gmail.com>
2025-01-11 17:57:07 +0200

    Facts: opening puzzles

--- src/components/Facts/Facts.tsx
+++ src/components/Facts/Facts.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { pluralize } from '../../utils';
 import Container from '../Container';
 import Puzzle from '../ui/Puzzle';
@@ -7,9 +8,26 @@ import './Facts.scss';
 import Dawn from '../ui/Dawn';
 
 function Facts({ className, ...props }: TFactsProps): JSX.Element {
+  const [openedFact, setOpenedFact] = useState(-1);
   const quantityFacts = FACTS.length;
   const facts = FACTS.map((item, index) => ({ ...item, num: ++index }));
   const factsDouble = facts.concat(facts);
+
+  function handlePointerDownPuzzle(indexPuzzle: number) {
+    return function (e: React.PointerEvent<HTMLDivElement>) {
+      e.preventDefault();
+      e.currentTarget.setPointerCapture(e.pointerId);
+      setOpenedFact(indexPuzzle);
+    };
+  }
+
+  function handlePointerUpPuzzle(indexPuzzle: number) {
+    if (indexPuzzle !== openedFact) return;
+
+    return function () {
+      setOpenedFact(-1);
+    };
+  }
 
   return (
     <Container
@@ -23,15 +41,22 @@ function Facts({ className, ...props }: TFactsProps): JSX.Element {
       <p className="facts__explain">
         зажмите любой из пазлов, удерживайте и читайте
       </p>
-      <ul className="facts__list">
+      <ul
+        className={['facts__list', openedFact !== -1 && 'facts__list_opened']
+          .filter(Boolean)
+          .join(' ')}
+      >
         {factsDouble.map(({ variant, text, num }, index) => (
           <li key={index} className="facts__item">
             <Puzzle
               className="facts__puzzle"
               variant={variant}
               text={text}
-              isOpen={false}
+              isOpen={openedFact === index}
               num={num}
+              onPointerDown={handlePointerDownPuzzle(index)}
+              onPointerUp={handlePointerUpPuzzle(index)}
+              onLostPointerCapture={handlePointerUpPuzzle(index)}
             />
             <Dawn className="facts__dawn" />
           </li>
