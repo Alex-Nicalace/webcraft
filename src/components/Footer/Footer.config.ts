9bc904e52b9a0243adb7cfa2008d4bf093437988
Alex-Nicalace <anicalace@gmail.com>
2025-01-12 15:31:37 +0200

    Footer: create & usage

---
+++ src/components/Footer/Footer.config.ts
@@ -0,0 +1,33 @@
+export const MY_CONTACTS = [
+  {
+    label: 'Telegram',
+    href: 'https://t.me/Nicalace',
+    type: 'Telegram',
+  },
+  {
+    label: 'anicalace@gmail.com',
+    href: 'mailto:anicalace@gmail.com',
+    type: 'почта',
+  },
+  {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/iskander8997/',
+    type: 'Instagram',
+  },
+  {
+    label: 'github',
+    href: 'https://github.com/Alex-Nicalace',
+    type: 'github',
+  },
+  {
+    label: '+373 (777) 3 55 37',
+    href: 'tel:+37377735537',
+    type: 'телефон',
+  },
+] as const;
+
+export const DESIGN_CONTACT = {
+  label: 'Design by Anna Privalova',
+  href: 'https://www.behance.net/anna_privalova_10',
+  type: 'behance',
+};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,54 @@
-// type TFooterProps = { }
-function Footer(/*{ }: TFooterProps*/): JSX.Element {
-  return <footer>footer</footer>;
+import Container from '../Container';
+import ContactPuzzle, { TContactPuzzleProps } from '../ui/ContactPuzzle';
+import { useScreenWidth } from '../../Context/ScreenWidthContext';
+import { DESIGN_CONTACT, MY_CONTACTS } from './Footer.config';
+import './Footer.scss';
+
+function getVariant(index: number): TContactPuzzleProps['variant'] {
+  if (index >= 0 && index < 6) {
+    return (index + 1) as TContactPuzzleProps['variant'];
+  }
+  throw new Error('Index of puzzle out of bounds');
+}
+
+function Footer(): JSX.Element {
+  const { isLessMobileMd } = useScreenWidth();
+
+  return (
+    <Container tag="footer" className="footer">
+      <div
+        className={['footer__links', isLessMobileMd && 'footer__links_column']
+          .filter(Boolean)
+          .join(' ')}
+      >
+        <address className="footer__address">
+          {MY_CONTACTS.map(({ label, href, type }, index) => (
+            <ContactPuzzle
+              key={href}
+              href={href}
+              target={['почта', 'телефон'].includes(type) ? '_self' : '_blank'}
+              rel="noopener noreferrer"
+              variant={getVariant(index)}
+              isSingleRow={isLessMobileMd}
+              aria-label={`Перейти на ${type || label}`}
+            >
+              {label}
+            </ContactPuzzle>
+          ))}
+        </address>
+        <ContactPuzzle
+          href={DESIGN_CONTACT.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          variant={6}
+          isSingleRow={isLessMobileMd}
+          aria-label={`Перейти на ${DESIGN_CONTACT.type || DESIGN_CONTACT.label}`}
+        >
+          {DESIGN_CONTACT.label}
+        </ContactPuzzle>
+      </div>
+    </Container>
+  );
 }
 
 export default Footer;
