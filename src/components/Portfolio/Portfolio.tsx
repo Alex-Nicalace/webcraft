cb667dad722f84308c591a79e13c4381aabb73c2
Alex-Nicalace <anicalace@gmail.com>
2025-01-14 10:11:40 +0200

    Portfolio: add Button

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,9 @@
 import Container from '../Container';
+import Button from '../ui/Button';
 import ProjectList from '../ui/ProjectList';
 import { PROJECTS } from './Portfolio.config';
 import { TPortfolioProps } from './Portfolio.types';
+import './Portfolio.scss';
 
 function Portfolio({ className, ...props }: TPortfolioProps): JSX.Element {
   return (
@@ -11,6 +13,9 @@ function Portfolio({ className, ...props }: TPortfolioProps): JSX.Element {
       {...props}
     >
       <ProjectList className="portfolio__projects" data={PROJECTS} />
+      <Button className="portfolio__contact-me" href="#my-contacts">
+        Связаться со мной
+      </Button>
     </Container>
   );
 }
