690d2da85eb47c925b3e182a800fc7c810bc3576
Alex-Nicalace <anicalace@gmail.com>
2025-01-19 11:04:20 +0200

    useIntersectionObserver: improvements

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,30 +1,49 @@
-import { useEffect, useState } from 'react';
+import { RefObject, useEffect, useState } from 'react';
+
+type UseIntersectionObserverOptions = IntersectionObserverInit & {
+  once?: boolean;
+};
 
 export function useIntersectionObserver(
-  target: Element | null | string | undefined,
-  options?: IntersectionObserverInit
+  target: RefObject<HTMLElement> | string | null | undefined,
+  options?: UseIntersectionObserverOptions
 ) {
   const [isIntersecting, setIsIntersecting] = useState(false);
+  const { once, ...rest } = options ?? {};
+  const ignore = once && isIntersecting;
 
-  useEffect(function manageScrolledState() {
-    if (!target) return;
-    const element =
-      typeof target === 'string' ? document.querySelector(target) : target;
-    if (!element) return;
+  useEffect(
+    function manageStateIntersecting() {
+      if (!target || ignore) return;
 
-    const callback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
+      const element =
+        typeof target === 'string'
+          ? document.querySelector(target)
+          : target.current;
+
+      if (!element) {
+        console.warn('Target element not found for useIntersectionObserver.');
+        return;
+      }
+
+      const callback: IntersectionObserverCallback = (entries, observer) => {
+        const [entry] = entries;
         setIsIntersecting(entry.isIntersecting);
-      });
-    };
 
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(element);
+        if (once && entry.isIntersecting) {
+          observer.unobserve(element);
+        }
+      };
 
-    return () => {
-      observer.unobserve(element);
-    };
-  });
+      const observer = new IntersectionObserver(callback, rest);
+      observer.observe(element);
+
+      return () => {
+        observer.unobserve(element);
+      };
+    },
+    [target, ignore, once, rest]
+  );
 
   return isIntersecting;
 }
