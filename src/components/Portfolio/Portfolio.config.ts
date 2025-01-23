a4596bbe2424ab97fc3c71e30cc017a32e842b0c
Alex-Nicalace <anicalace@gmail.com>
2025-01-23 15:42:33 +0200

    Portfolio.config: add links & img

--- src/components/Portfolio/Portfolio.config.ts
+++ src/components/Portfolio/Portfolio.config.ts
@@ -1,40 +1,57 @@
-import projectImg from '../../assets/img/projects/Снимок экрана 2024-11-30 150026.jpg';
+import screenshot1 from '../../assets/img/projects/01.jpg';
+import screenshot2 from '../../assets/img/projects/02.jpg';
+import screenshot3 from '../../assets/img/projects/03.jpg';
+import screenshot4 from '../../assets/img/projects/04.jpg';
+import screenshot5 from '../../assets/img/projects/05.jpg';
+import screenshot6 from '../../assets/img/projects/06.jpg';
 
 export const PROJECTS = [
   {
-    url: '#1',
-    title:
-      'Очень длинное название проекта в две строки и не влазит чень длинное название проекта в две строки и не влазит',
+    url: 'http://commerce.alex-webdev.ru/',
+    title: 'Интернет-магазин одежды',
     popupData: {
       title: 'Название организации',
-      imageUrl: projectImg,
+      imageUrl: screenshot1,
     },
   },
   {
-    url: '#2',
-    title:
-      'Очень длинное название проекта в две строки и не влазит чень длинное название проекта в две строки и не влазит',
+    url: 'http://build.alex-webdev.ru/',
+    title: 'Сайт для строительной компании',
     popupData: {
       title: 'Название организации',
-      imageUrl: projectImg,
+      imageUrl: screenshot2,
     },
   },
   {
-    url: '#3',
-    title:
-      'Очень длинное название проекта в две строки и не влазит чень длинное название проекта в две строки и не влазит',
+    url: 'http://stylish-image.alex-webdev.ru/',
+    title: 'Интернет-магазин одежды',
     popupData: {
       title: 'Название организации',
-      imageUrl: projectImg,
+      imageUrl: screenshot3,
     },
   },
   {
-    url: '#4',
-    title:
-      'Очень длинное название проекта в две строки и не влазит чень длинное название проекта в две строки и не влазит',
+    url: 'https://the-wild-oasis-mocha-gamma.vercel.app',
+    title: 'Дашборд по бронированию отеля',
     popupData: {
       title: 'Название организации',
-      imageUrl: projectImg,
+      imageUrl: screenshot4,
+    },
+  },
+  {
+    url: 'http://consulting.alex-webdev.ru',
+    title: 'Сайт для консалтинговой компании в сфере фармацевтики',
+    popupData: {
+      title: 'Название организации',
+      imageUrl: screenshot5,
+    },
+  },
+  {
+    url: 'http://sea-battle.alex-webdev.ru',
+    title: 'Игра',
+    popupData: {
+      title: 'Название организации',
+      imageUrl: screenshot6,
     },
   },
 ];
