975f5b6a3c1b1a800c14541be7374762cf4b61c4
Alex-Nicalace <anicalace@gmail.com>
2025-01-25 13:34:33 +0200

    useRefs: create

---
+++ src/hooks/useRefs.ts
@@ -0,0 +1,29 @@
+import { useCallback, useRef } from 'react';
+
+/**
+ * Возвращает массив из двух элементов:
+ * 1. ссылки на Map, хранящий ссылки на DOM-элементы,
+ *    по которым можно обратиться по ключу.
+ *    Ключ - это React.Key, по которому можно найти элемент.
+ *    map.current.get(key) - получить ссылку на DOM-элемент.
+ * 2. функцию setRef, которая позволяет добавить/удалить
+ *    ссылки на DOM-элементы в Map.
+ *    setRef(key, node) - добавить/удалить ссылку на DOM-элемент.
+ *    key - React.Key, по которому можно найти элемент.
+ *    node - DOM-элемент, на который нужно добавить ссылку
+ *          (если null, то ссылка будет удалена).
+ */
+export function useRefs<T extends HTMLElement>() {
+  const itemsRef = useRef<Map<React.Key, T>>(new Map());
+
+  const setRef = useCallback((key: React.Key, node: T | null) => {
+    const map = itemsRef.current;
+    if (node) {
+      map.set(key, node);
+    } else {
+      map.delete(key);
+    }
+  }, []);
+
+  return [itemsRef, setRef] as const;
+}
