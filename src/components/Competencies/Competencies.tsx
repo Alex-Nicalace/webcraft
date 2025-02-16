bc39671010fe54604449caf55ce2a152264db7f3
Alex-Nicalace <anicalace@gmail.com>
2025-02-16 14:23:20 +0200

    Competencies: usage remote data

--- src/components/Competencies/Competencies.tsx
+++ src/components/Competencies/Competencies.tsx
@@ -1,20 +1,18 @@
-import Container from '../Container';
+import { useFetch } from '../../hooks/useFetch';
 import { TCompetenciesProps } from './Competencies.types';
+import Container from '../Container';
+import Loader from '../ui/Loader';
+import ErrorMessage from '../ui/ErrorMessage';
 import './Competencies.scss';
-
-const COMPETENCIES = [
-  'Javascript',
-  'React JS',
-  'Typescript',
-  'html',
-  'css',
-  'scss',
-];
 
 function Competencies({
   className,
   ...props
 }: TCompetenciesProps): JSX.Element {
+  const [{ responseData: competencies, isLoading, errorMessage }] = useFetch<
+    string[]
+  >('/data/competencies.json');
+
   return (
     <Container
       tag="section"
@@ -22,13 +20,19 @@ function Competencies({
       {...props}
     >
       <h2 className="competencies__heading heading">Мои компетенции</h2>
-      <ul className="competencies__list">
-        {COMPETENCIES.map((item, index) => (
-          <li key={index} className="competencies__item text-competencies">
-            <div className="competencies__competence">{item}</div>
-          </li>
-        ))}
-      </ul>
+      {isLoading && <Loader />}
+      {errorMessage && !isLoading && (
+        <ErrorMessage message="Компетенции не загрузились" />
+      )}
+      {!errorMessage && !isLoading && (
+        <ul className="competencies__list">
+          {(competencies ?? []).map((item, index) => (
+            <li key={index} className="competencies__item text-competencies">
+              <div className="competencies__competence">{item}</div>
+            </li>
+          ))}
+        </ul>
+      )}
     </Container>
   );
 }
