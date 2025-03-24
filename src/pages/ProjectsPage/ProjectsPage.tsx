f9e54cf0ad5c9036bd5ede1f2fd4fc5f5c3747ee
Alex-Nicalace <anicalace@gmail.com>
2025-03-24 09:46:48 +0200

    ProjectsPage: create

---
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,14 @@
+import './ProjectsPage.scss';
+// type TProjectsPageProps = { }
+function ProjectsPage(/*{ }: TProjectsPageProps*/): JSX.Element {
+  return (
+    <div>
+      {' '}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quaerat,
+      aliquam minus voluptatum tenetur saepe facere enim ducimus eos in non quod
+      adipisci magni quae dolorem et optio voluptatibus repudiandae.
+    </div>
+  );
+}
+
+export default ProjectsPage;
