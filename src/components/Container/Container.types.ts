fb3a33cf3400ed6140eca802726cd16ec342fc49
Alex-Nicalace <anicalace@gmail.com>
2025-04-06 11:08:06 +0300

    Container:  refactoring

--- src/components/Container/Container.types.ts
+++ src/components/Container/Container.types.ts
@@ -1,6 +1,3 @@
-import React from 'react';
-
 export type TContainerProps<T extends keyof JSX.IntrinsicElements> = {
-  children: React.ReactNode;
   tag: T; // Ограничение по ключам JSX.IntrinsicElements
 } & JSX.IntrinsicElements[T]; // Атрибуты соответствующего элемента
